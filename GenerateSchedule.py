b1a8f63f2deab379e911e01acddc179b8d7c41c6
jmaker <jwerner@gmail.com>
2016-08-22 11:24:52 -0500

    Closer to initial class scoring

--- GenerateSchedule.py
+++ GenerateSchedule.py
@@ -27,8 +27,18 @@ studentsKey = input["StudentsKey"]
 
 allPrefs = {}
 
+classScores = [[0 for i in range(0,numClasses)] for j in range(0,numSections)]
+
 for studentNum in range(0,numStudents):
-	print str(studentNum) + ":"
-	print input[studentsKey][str(studentNum)][classesKey]
-	print input[studentsKey][str(studentNum)][sectionsKey]
+	#print str(studentNum) + ":"
+	#print input[studentsKey][str(studentNum)][classesKey]
+	#print input[studentsKey][str(studentNum)][sectionsKey]
+	for classNum in range(0,numClasses):
+		for sectionNum in range(0,numSections):
+			if(input[studentsKey][str(studentNum)][str(sectionsKey)][str(sectionNum)] == 1):
+				classScores[classNum][sectionNum] += input[studentsKey][str(studentNum)][str(classesKey)][str(classNum)]
+
+
+print classScores
+		
 
