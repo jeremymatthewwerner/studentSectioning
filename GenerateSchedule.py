b8aa1b6119e7a911a974415e7ac5502a592d7d43
jmaker <jwerner@gmail.com>
2016-08-23 11:33:08 -0500

    starting the schedule scoring  function

--- GenerateSchedule.py
+++ GenerateSchedule.py
@@ -7,14 +7,8 @@ import numpy as np
 
 import pprint
 
-
-def get_pretty_print(json_object):
-    return json.dumps(json_object, sort_keys=True, indent=4, separators=(',', ': '))
-
-random.seed()
-
 with open(sys.argv[1]) as data_file:    
-    input = json.load(data_file)
+	input = json.load(data_file)
 
 numClasses = input["NumClasses"]
 numSections = input["NumSections"]
@@ -26,7 +20,23 @@ sectionsKey = input["SectionsKey"]
 classesKey = input["ClassesKey"]
 studentsKey = input["StudentsKey"]
 
+def get_pretty_print(json_object):
+	return json.dumps(json_object, sort_keys=True, indent=4, separators=(',', ': '))
+
+def studentScheduleScore(student, schedule):
+	return 3
+
+def scheduleScore(schedule, inputJson):
+	scores = []
+	for studentNum in range(0,numStudents):
+		scores.append(studentScheduleScore(inputJson[studentsKey][str(studentNum)], schedule))
+	return np.mean(scores)
+
+random.seed()
+
 allPrefs = {}
+
+# Build an initial schedule out of most popular classes (1st track highest rated, 2nd 2nd, etc.)
 
 classScores = [[0 for i in range(0,numClasses)] for j in range(0,numSections)]
 
@@ -38,7 +48,7 @@ for studentNum in range(0,numStudents):
 			if(input[studentsKey][str(studentNum)][str(sectionsKey)][str(sectionNum)] == 1):
 				classScores[sectionNum][classNum] += input[studentsKey][str(studentNum)][str(classesKey)][str(classNum)]	
 
-print classScores
+#print classScores
 
 for sectionNum in range(0,numSections):
 	arr = np.array(classScores[sectionNum])
@@ -46,9 +56,6 @@ for sectionNum in range(0,numSections):
 	for trackNum in range(0, numTracks):
 		schedule[sectionNum][trackNum] = top3[trackNum]
 
-print schedule
+print "Schedule:" + str(schedule)
 
-
-
-		
-
+print "Schedule score: " + str(scheduleScore(schedule, input))
